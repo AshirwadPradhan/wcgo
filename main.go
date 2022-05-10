28d69a82dcb65aaaa41020f3c1951c3c674878de
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2022-05-11 01:51:57 +0530

    added bytes count

--- main.go
+++ main.go
@@ -10,15 +10,22 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	flag.Parse()
-	fmt.Println(wc(os.Stdin, *lines))
+	fmt.Println(wc(os.Stdin, *lines, *bytes))
 }
 
-func wc(r io.Reader, countLines bool) int {
+func wc(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
+	} else if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	} else if countBytes && countLines {
+		fmt.Printf("Cannot specify both flags at once!!!")
+		os.Exit(1)
 	}
 
 	count := 0
--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCountWords(t *testing.T) {
 	scannedString := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	count := wc(scannedString, false)
+	count := wc(scannedString, false, false)
 
 	if exp != count {
 		t.Errorf("Expected %d, got %d instead !!!\n", exp, count)
@@ -18,7 +18,17 @@ func TestCountWords(t *testing.T) {
 func TestCountLines(t *testing.T) {
 	scannedString := bytes.NewBufferString("word1 word2 word3 word4\n nextline nextline1 \n nextline1 nextline2 \n")
 	exp := 3
-	count := wc(scannedString, true)
+	count := wc(scannedString, true, false)
+
+	if exp != count {
+		t.Errorf("Expected %d, got %d instead !!!\n", exp, count)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	scannedString := bytes.NewBufferString("word1\n")
+	exp := 6
+	count := wc(scannedString, false, true)
 
 	if exp != count {
 		t.Errorf("Expected %d, got %d instead !!!\n", exp, count)
